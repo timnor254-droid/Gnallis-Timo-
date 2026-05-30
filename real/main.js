fa1abea8d2944b6a806158fdbc7ba25cc7f9d4b0
timnor254-droid <timnor254@edu.linkoping.se>
2026-05-30 12:34:05 +0000

    f

--- real/main.js
+++ real/main.js
@@ -4,3 +4,38 @@ const meny = document.querySelector(".meny");
 idmeny.addEventListener("click", () => {
     meny.classList.toggle("aktiv");
 });
+
+const thumbs = document.querySelectorAll('.thumb');
+
+
+const lightbox = document.querySelector('#lightbox');
+
+const lightboxImg = document.querySelector('#lightbox-img');
+
+const closeBtn = document.querySelector('#close-btn');
+
+const galleriBilder = document.querySelectorAll('.bild-gallery img');
+
+galleriBilder.forEach(img => {
+  img.addEventListener('click', () => {
+    const fullSizeSrc = img.getAttribute('src');
+    lightboxImg.src = fullSizeSrc;
+    lightbox.classList.remove('lightbox-hidden');
+    lightbox.classList.add('lightbox-visible');
+  });
+});
+const closeLightbox = () => {
+  lightbox.classList.remove('lightbox-visible');
+  lightbox.classList.add('lightbox-hidden');
+  lightboxImg.src = "";
+};
+closeBtn.addEventListener('click', closeLightbox);
+lightbox.addEventListener('click', (event) => {
+
+  if (event.target === lightbox) {
+
+    closeLightbox();
+
+  }
+
+});
